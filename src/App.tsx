210055becfbf02ae7f6537e004369fb8606a309f
l33th <l33th@protonmail.ch>
2021-11-20 06:33:10 +0200

    quizzer setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This app is built with ReactJS, TypeScript and Styled Components.</p>
+      Quizzer React-TypeScript App
+    </div>
+  );
+}
+
+export default App;
