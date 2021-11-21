c9c0fe91db2503165189c3c90797146f030bc31d
l33th <l33th@protonmail.ch>
2021-11-21 05:49:11 +0200

    created answer object in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
@@ -11,7 +18,7 @@ const App = () => {
   const initialValue: number = 0;
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(initialValue);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(initialValue);
