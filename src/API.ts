261562cd080ba8ad9ca6320703fadda43b9e99ad
l33th <l33th@protonmail.ch>
2021-11-21 05:43:45 +0200

    map over data

--- src/API.ts
+++ src/API.ts
@@ -1,22 +1,34 @@
+import QuestionCard from './components/QuestionCard';
+import { shuffleArray } from './utils';
+
 export type Question = {
   category: string;
   correct_answer: string;
   difficulty: string;
-  incorrect_answer: string;
+  incorrect_answer: string[]; // this was originally a string but gave an error
   question: string;
   type: string;
 };
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
   HARD = 'hard',
 }
+
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([
+      ...question.incorrect_answer, // this gave an error when its type was string instead of string[]
+      question.correct_answer,
+    ]),
+  }));
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,3 @@
-export const Utils = {};
+// Not completely accurate, but it'll do
+export const shuffleArray = (array: any[]) =>
+  [...array].sort(() => Math.random() - 0.5);
