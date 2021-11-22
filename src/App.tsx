a5c18f2f0f9ef01f7018b6bfd9389f1a5155b49f
l33th <l33th@protonmail.ch>
2021-11-22 17:31:45 +0200

    nextQuestion function starter setup

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,14 @@ const App = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      
+    }
+  };
 
   return (
     <div>
